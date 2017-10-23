e826c59a9765be88a66bad6dad6221605d54771a
DejEludire <faeludire@gmail.com>
2017-10-23 18:52:10 +0100

    Update

---
+++ get_last_tweet_id.py
@@ -0,0 +1,9 @@
+# Import our Twitter credentials from credentials.py
+from credentials import *
+import tweepy
+from time import sleep
+
+# Access and authorize our Twitter credentials from credentials.py
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
